6594da5094081bcf2580d8a92544963bab085580
uri khaimov <urikhaimov@gmail.com>
2021-11-08 09:12:06 +0200

    add tests

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from '../App';
+
+test('render App', () => {
+  const component = renderer.create(
+    <App/>,
+
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+ 
+});
